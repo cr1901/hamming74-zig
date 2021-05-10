1d536b997f4df32b681b6812f45c2a7336584579
William D. Jones <thor0505@comcast.net>
2021-05-10 01:42:51 -0400

    Begin implementing multiply, implement elementAt.

--- src/gf2mat.zig
+++ src/gf2mat.zig
@@ -6,8 +6,8 @@ const testing = std.testing;
 
 
 /// Row major-only
-/// Most Significant Bit to Least Significant Bit order per byte, going in order of rows.
-/// Least Significant Byte of data store used first.
+/// The first bit in a row is stored at the Most Significant Bit available in the current byte.
+/// Least Significant Byte of data store array is used first.
 pub fn Gf2Mat(
     comptime r: comptime_int,
     comptime c: comptime_int,
@@ -48,14 +48,63 @@ pub fn Gf2Mat(
             return out;
         }
 
+        pub fn elementAt(self: Self, curr_row: usize, curr_col: usize) u1 {
+            var bit_idx: usize = curr_row * cols + curr_col;
+            var byte_idx: usize = bit_idx/8;
+
+            var bit_idx_in_byte: u3 = 7 - @intCast(u3, bit_idx % 8);
+            var bit_mask: u8 = @as(u8, 1) << bit_idx_in_byte;
+
+            var curr_byte: u8 = self.data[byte_idx];
+            var curr_bit: u1 = @intCast(u1, (curr_byte & bit_mask) >> bit_idx_in_byte);
+
+            return curr_bit;
+        }
+
         pub fn multiply(self: Self, b: anytype) Gf2Mat(Self.rows, @TypeOf(b).cols) {
             if(Self.cols != @TypeOf(b).rows) {
                 @compileError("Self.cols does not match b.rows");
             }
 
+            const mult_bit_size: comptime_int = c * r;
+            var mul_data: [mult_bit_size]u1 = [_]u1{0} ** mult_bit_size;
+
             @panic("unimplemented");
         }
     };
+}
+
+test "elementAt" {
+    const test_mat = Gf2Mat(4, 5).fromBitArray([_]u1 {
+        1, 1, 0, 1, 1,
+        1, 0, 1, 1, 0,
+        1, 0, 0, 0, 1,
+        0, 1, 1, 1, 0
+    });
+
+    testing.expectEqual(test_mat.elementAt(0, 0), 1);
+    testing.expectEqual(test_mat.elementAt(0, 1), 1);
+    testing.expectEqual(test_mat.elementAt(0, 2), 0);
+    testing.expectEqual(test_mat.elementAt(0, 3), 1);
+    testing.expectEqual(test_mat.elementAt(0, 4), 1);
+
+    testing.expectEqual(test_mat.elementAt(1, 0), 1);
+    testing.expectEqual(test_mat.elementAt(1, 1), 0);
+    testing.expectEqual(test_mat.elementAt(1, 2), 1);
+    testing.expectEqual(test_mat.elementAt(1, 3), 1);
+    testing.expectEqual(test_mat.elementAt(1, 4), 0);
+
+    testing.expectEqual(test_mat.elementAt(2, 0), 1);
+    testing.expectEqual(test_mat.elementAt(2, 1), 0);
+    testing.expectEqual(test_mat.elementAt(2, 2), 0);
+    testing.expectEqual(test_mat.elementAt(2, 3), 0);
+    testing.expectEqual(test_mat.elementAt(2, 4), 1);
+
+    testing.expectEqual(test_mat.elementAt(3, 0), 0);
+    testing.expectEqual(test_mat.elementAt(3, 1), 1);
+    testing.expectEqual(test_mat.elementAt(3, 2), 1);
+    testing.expectEqual(test_mat.elementAt(3, 3), 1);
+    testing.expectEqual(test_mat.elementAt(3, 4), 0);
 }
 
 test "mul" {
